02657f75fbe2acaae6add417caf3a21baae47799
Michele Cotrufo <michele.cotrufo@gmail.com>
2022-10-03 17:53:30 -0400

    check that PyQt5 is installed

--- pyThorlabsPM100x/__init__.py
+++ pyThorlabsPM100x/__init__.py
@@ -1,4 +1,9 @@
-from .main import interface, gui
+#If this package was installed only to use the low-level driver, the PyQt library is not necessarily installed. In this case, importing stuff from main.py would generate an error
+import importlib.util
+package_name = 'PyQt5'
+spec = importlib.util.find_spec(package_name)
+if spec:
+    from .main import interface, gui
 
 
 
