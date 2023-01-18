79040e60e9b82320119f422c2a1c9e362e7751e4
Michele Cotrufo <michele.cotrufo@gmail.com>
2023-01-18 11:21:57 -0500

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements_strict.txt") as f:
     required_packages = f.read().splitlines()
 
 setuptools.setup(name='pyThorlabsPM100x',
-      version='0.23',
+      version='0.25r',
       description='A python library/GUI to access and control the PM100x consoles of Thorlabs.',
       long_description=long_description,
       long_description_content_type='text/markdown',
